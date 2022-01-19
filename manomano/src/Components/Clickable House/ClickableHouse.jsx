621ae0d58e2e6de9cfc02a140c4358a70b878da2
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-01-19 15:47:20 +0100

    Created Component

---
+++ manomano/src/Components/Clickable House/ClickableHouse.jsx
@@ -0,0 +1,9 @@
+import styles from "./ClickableHouse.module.css";
+
+export default function ClickableHouse() {
+  return (
+    <div>
+      <h2 className={styles.title}>Je suis la maison</h2>
+    </div>
+  );
+}
--- manomano/src/Screens/Home/Home.jsx
+++ manomano/src/Screens/Home/Home.jsx
@@ -1,9 +1,12 @@
+import ClickableHouse from "../../Components/Clickable House/ClickableHouse";
+
 import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <h1 className={styles.title}>Je suis le Home</h1>
+      <ClickableHouse />
     </div>
   );
 }
