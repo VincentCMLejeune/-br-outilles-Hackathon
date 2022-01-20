9783bcf29d1fed5624c113bd4d8971b2542c197a
VincentCMLejeune <vincentcmlejeune@gmail.com>
2022-01-20 17:24:51 +0100

    Meow

---
+++ manomano/src/Screens/Product Filter/ProductFilter.jsx
@@ -0,0 +1,9 @@
+import styles from "./ProductFilter.module.css";
+
+export default function ProductFilter() {
+  return (
+    <div className={styles.productFilterContainer}>
+      <h1>I'm the product filter</h1>
+    </div>
+  );
+}
