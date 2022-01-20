baed0a4095d8a009f2dfbbd4b1c7d9e71ae3b594
Mel <melissa.olas@gmail.com>
2022-01-20 11:32:25 +0100

    adding a card

--- manomano/src/Screens/Search/Card.jsx
+++ manomano/src/Screens/Search/Card.jsx
@@ -3,6 +3,7 @@ import styles from "./Search.module.css";
 import chainSaw from "../../Assets/Tronçonneuse.jpg";
 import timberpro from "../../Assets/timberproLogo.png";
 import stars from "../../Assets/stars.png";
+import price from "../../Assets/price.png";
 
 export default function Card() {
     return (
@@ -14,6 +15,7 @@ export default function Card() {
             <div className={styles.rightSide}>
                 <h1 className={styles.cardTitle}>Tronçonneuse thermique 62 cm3, guide 50 cm, 2 chaines + housse de transport</h1>
                 <img className={styles.starsImg} src={stars} alt="stars"/>
+                <img className={styles.price} src={price} alt="price"/>
             </div>
         </div>
     )
